0a0d0397aee6e164b698c86c9705e26ecfcb2018
sachinmurali <sachin@lob.com>
2020-10-05 09:36:04 -0700

    feat(movies): add list and retrieve movies endpoints

---
+++ internal/test/routes.go
@@ -0,0 +1,23 @@
+// internal/test/routes.go
+package test
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/labstack/echo"
+)
+
+// FilterRoutes takes in a slice of Echo routes and returns a slice of strings
+// with handler routes.
+func FilterRoutes(routes []*echo.Route) []string {
+	rs := []string{}
+
+	for _, r := range routes {
+		if strings.Contains(r.Name, "*handler") {
+			rs = append(rs, fmt.Sprintf("%s %s", r.Method, r.Path))
+		}
+	}
+
+	return rs
+}
